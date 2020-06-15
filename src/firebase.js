31e82d0e1e7b0ec5321dbc4fc89959a346bf23f1
Matthew Hernandez <matt.hernandez94@gmail.com>
2020-06-15 17:25:12 -0400

    imported firebase realtime database

---
+++ src/firebase.js
@@ -0,0 +1,20 @@
+// firebase.js
+import firebase from 'firebase/app';
+
+// initialize firebase
+// config object
+var firebaseConfig = {
+  apiKey: "AIzaSyAOYShPCmJls_nddDt39UBjTHRhJFMyjjo",
+  authDomain: "tracked-70405.firebaseapp.com",
+  databaseURL: "https://tracked-70405.firebaseio.com",
+  projectId: "tracked-70405",
+  storageBucket: "tracked-70405.appspot.com",
+  messagingSenderId: "998621565378",
+  appId: "1:998621565378:web:85e7f57765cec8de40065b"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+// exports the configured version of firebase
+export default firebase;
